f8f78d2e396040cfb6403e07d10baecc18be0347
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-09 18:08:50 +0530

    Retrive selected records

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -10,11 +10,27 @@ namespace ProductReviewManagement
     {
         public readonly DataTable dataTable = new DataTable();
 
+        //UC2
         public void TopRecords(List<ProductReview> listProductReview)
         {
             var recordedData = (from productReviews in listProductReview
                                 orderby productReviews.Rating descending
                                 select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
+            }
+        }
+        //UC3
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProductId == 1 ||
+                                     productReviews.ProductId == 4 ||
+                                     productReviews.ProductId == 9) && (productReviews.Rating > 3)
+                               select productReviews;
+
             foreach (var list in recordedData)
             {
                 Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -34,7 +34,13 @@ namespace ProductReviewManagement
                 Console.WriteLine("ProductId:- " + list.ProductId + " " + "UserId:- " + list.UserId
                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike :- " + list.isLike);
             }
+            Management management = new Management();
+            //UC2
+            management.TopRecords(productReviewList);
 
+            //UC3
+            management.SelectedRecords(productReviewList);
         }
     }
 }
+    
